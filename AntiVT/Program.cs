dd078ec69c324e4b5f82887320bcd17567dbd69c
User <User@HOME-PC>
2023-06-01 22:46:20 +0300

    Добавьте файлы проекта.

---
+++ AntiVT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace AntiVT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (BaseBoardSerial() == true)
+            {
+                Console.WriteLine("This is not VT machine");
+            }
+        }
+        public static bool BaseBoardSerial()
+        {
+            foreach (string file in Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)))
+            {
+                if (!file.Contains("Acrobat Reader") & !file.Contains(".pdf"))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
